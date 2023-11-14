511c21aa2149307d68436a4068b3653c0a13b7a4
rivques <38469076+rivques@users.noreply.github.com>
2023-11-14 10:32:34 -0500

    made the thing

--- reciever.py
+++ reciever.py
@@ -4,6 +4,15 @@ import ipaddress
 from adafruit_httpserver import Server, Request, Response
 import socketpool
 import microcontroller
+import digitalio
+import board
+
+blue_pin = digitalio.DigitalInOut(board.GP14)
+green_pin = digitalio.DigitalInOut(board.GP15)
+blue_pin.direction = digitalio.Direction.OUTPUT
+green_pin.direction = digitalio.Direction.OUTPUT
+
+blue_pin.value = True
 
 ap_addr = ipaddress.IPv4Address("192.168.1.1")
 ap_netmask = ipaddress.IPv4Address("255.255.255.0")
@@ -21,17 +30,29 @@ server = Server(pool, "/static", debug=True)
 def base(request: Request):  # pylint: disable=unused-argument
     #  serve the HTML f string
     #  with content type text/html
-    return Response(request, f"Hello world!", content_type='text/plain')
+    for _ in range(5):
+        green_pin.value = True
+        sleep(0.2)
+        green_pin.value = False
+        sleep(0.2)
+    for _ in range(5):
+        green_pin.value = True
+        blue_pin.value = True
+        sleep(0.1)
+        green_pin.value = False
+        blue_pin.value = False
+        sleep(0.1)
+    return Response(request, f"Doorbell triggered!", content_type='text/plain')
 
 try:
     server.start(str(ap_addr))
-    print("Listening on http://%s:80" % ap_addr.ipv4_address)
+    print(f"Listening on http://{ap_addr}:80")
 #  if the server fails to begin, restart the pico w
 except OSError:
     sleep(5)
     print("restarting..")
     microcontroller.reset()
-
+blue_pin.value = False
 while True:
     try:
         server.poll()
---
+++ transmitter.py
@@ -0,0 +1,47 @@
+import digitalio
+import board
+import time
+import ssl
+import wifi
+import socketpool
+import microcontroller
+import adafruit_requests
+import alarm
+
+blue_pin = digitalio.DigitalInOut(board.GP14)
+green_pin = digitalio.DigitalInOut(board.GP15)
+blue_pin.direction = digitalio.Direction.OUTPUT
+green_pin.direction = digitalio.Direction.OUTPUT
+
+green_pin.value = True
+blue_pin.value = True
+#  adafruit quotes URL
+target_url = "http://192.168.1.1"
+
+#  connect to SSID
+try:
+    wifi.radio.connect("ENGR_DOORBELL", "mrmiller")
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+except Exception as e:
+    print(f"error while connecting: {e}")
+    blue_pin.value = False
+    while True:
+        pass
+
+blue_pin.value = False
+green_pin.value = False
+if alarm.wake_alarm is not None:
+    blue_pin.value=True
+    print("Fetching text from %s" % target_url)
+    response = requests.get(target_url)
+    blue_pin.value=False
+    green_pin.value = True
+    print(f"Got response: {response.text}")
+    response.close()
+    time.sleep(5)
+    green_pin.value = False
+    alert_from_alarm_needed = False
+print("deep sleeping...")
+wifi.radio.stop_station()
+alarm.exit_and_deep_sleep_until_alarms(alarm.pin.PinAlarm(pin=board.GP20, value=False, pull=True))
